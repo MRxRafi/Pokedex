34a68eaad351ddb3f927e57531cdbd8695148dca
Rafael <38105703+MRxRafi@users.noreply.github.com>
2019-03-24 00:02:11 +0100

    Update PokedexXML.java

--- pokedexWS/src/main/java/es/urjc/gestiondatos/pokedex/PokedexXML.java
+++ pokedexWS/src/main/java/es/urjc/gestiondatos/pokedex/PokedexXML.java
@@ -3,7 +3,44 @@ package es.urjc.gestiondatos.pokedex;
 public class PokedexXML {
 	String num_serie;
 	String usuario;
-	//Y mas...
+	String color_cir;
 	
+	public PokedexXML() {}
+	
+	public PokedexXML(String num_serie, String usuario, String color_cir) {
+		this.num_serie = num_serie;
+		this.usuario = usuario;
+		this.color_cir = color_cir;
+	}
+
+	public String getNum_serie() {
+		return num_serie;
+	}
+
+	public void setNum_serie(String num_serie) {
+		this.num_serie = num_serie;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getColor_cir() {
+		return color_cir;
+	}
+
+	public void setColor_cir(String color_cir) {
+		this.color_cir = color_cir;
+	}
+
+	@Override
+	public String toString() {
+		return "{\"num_serie\":\""+this.num_serie+"\",\"usuario\":\""+this.usuario+"\",\"color_cir\":\""+this.color_cir+"\"}";
+	}
 	
 }
+
